33b0d0d8cc416c2e55ceae54cba5d867331c44e5
Gil Shotan <gilsho@cs.stanford.edu>
2013-03-01 17:53:02 -0800

    cleaned up vman.c and vman.h

--- pintos/src/vm/vman.c
+++ pintos/src/vm/vman.c
@@ -1,5 +1,4 @@
 #include "vm/vman.h"
-
 #include "userprog/process.h"
 #include <debug.h>
 #include <inttypes.h>
@@ -26,52 +25,30 @@
 #include "vm/mmap.h"
 #include "vm/swap.h"
 
-#if (DEBUG & DEBUG_STACK)
-#define PRINT_GROW_STACK(X) printf(X)
-#define PRINT_GROW_STACK_2(X,Y) {printf("vman_grow_stack: "); printf(X,Y);}
-#else
-#define PRINT_GROW_STACK(X) do {} while(0)
-#define PRINT_GROW_STACK_2(X,Y) do {} while(0)
-#endif
 
-#if (DEBUG & DEBUG_STACK)
-#define PRINT_LOAD_PAGE(X) printf(X)
-#define PRINT_LOAD_PAGE_2(X,Y) {printf("vman_load_page: "); printf(X,Y);}
-#else
-#define PRINT_LOAD_PAGE(X) do {} while(0)
-#define PRINT_LOAD_PAGE_2(X,Y) do {} while(0)
-#endif
+/* Virtual Memory Manager 
+	 ---------------------
+	 this module manages the virtual memory for a process.
+	 all request to allocate/deallocate memory should be tunneled
+	 through the interface provided by vman.h
+ */
 
-#if (DEBUG & DEBUG_MAP_SEGMENT)
-#define PRINT_MAP_SEGMENT_2(X,Y) {printf("vman_map_segment: "); printf(X,Y);}
-#else
-#define PRINT_MAP_SEGMENT_2(X,Y) do {} while (0)
-#endif
+extern struct lock lock_filesys; /* a coarse global lock for synchronizing access 
+				                            to the file system */
 
-#if (DEBUG & DEBUG_MAP_FILE)
-#define PRINT_MAP_FILE_2(X,Y) {printf("vman_map_file: "); printf(X,Y);}
-#else
-#define PRINT_MAP_FILE_2(X,Y) do {} while (0)
-#endif
+extern struct lock lock_frame;	 /* a lock for synchronizing access to the frame
+																		table */
 
-#if (DEBUG & DEBUG_UNMAP_FILE)
-#define PRINT_UNMAP_FILE_2(X,Y) {printf("vman_unmap_file: "); printf(X,Y);}
-#else
-#define PRINT_UNMAP_FILE_2(X,Y) do {} while (0)
-#endif
 
-extern struct lock lock_filesys; /* a coarse global lock restricting access 
-                            to the file system */
-extern struct lock lock_frame;
+void vman_load_page_helper(struct pagesup_entry *pse);
 
-void 
-vman_load_page_helper(struct pagesup_entry *pse);
-
+/* initialize the virtual memory manager */
 void vman_init(void)
 {
 	frame_init_table(); 
 }
 
+/* checks whether a region in virtual memory is unmapped. */
 bool 
 vman_upages_unmapped(void *upage_head, int npages)
 {
@@ -86,16 +63,13 @@ vman_upages_unmapped(void *upage_head, int npages)
 	return true;
 }
 
-/* install a mem-mapped file into the supplementary table,
-	 by determining the number of pages needed */
+/* creates a mapping in the page supplemental table for a mem-mapped file. 
+	 returns true if operation was successful */
 bool 
 vman_map_file(void *upage, struct file *file, uint32_t file_len)
 {
 	struct thread *t = thread_current();
 	int npages = num_pages(file_len);
-
-	PRINT_MAP_FILE_2("file_len: %d, ",file_len);
-	PRINT_MAP_FILE_2("npages: %d\n",npages);
 
 	if (!vman_upages_unmapped(upage, npages))
 		return false;
@@ -107,8 +81,6 @@ vman_map_file(void *upage, struct file *file, uint32_t file_len)
 		void *cur_upage = (void *)(((uint8_t *)upage) + i*PGSIZE);
 		off_t cur_offset = i*PGSIZE;
 		page_supplement_install_filepage(&t->pst,cur_upage,valid_bytes,file,cur_offset);
-		PRINT_MAP_FILE_2("cur_upage: %p, ",cur_upage);
-		PRINT_MAP_FILE_2("bytes_remaining: %d\n",bytes_remaining);
 		bytes_remaining -= valid_bytes;
 	}
 	ASSERT(bytes_remaining == 0);
@@ -116,6 +88,8 @@ vman_map_file(void *upage, struct file *file, uint32_t file_len)
 	return true;
 }
 
+/* creates a mapping in the supplemental table for a data segment. returns true
+   if the operation was successful. */
 bool 
 vman_map_segment (void *upage, struct file *file, off_t offset, int init_data_bytes, 
 									int uninit_data_bytes, bool writable) 
@@ -123,10 +97,6 @@ vman_map_segment (void *upage, struct file *file, off_t offset, int init_data_by
 	struct thread *t = thread_current();
 	int total_bytes = init_data_bytes + uninit_data_bytes;
 	int npages = num_pages(total_bytes);
-
-	PRINT_MAP_SEGMENT_2("init_data_bytes: %d, ",init_data_bytes);
-	PRINT_MAP_SEGMENT_2("uninit_data_bytes: %d, ",uninit_data_bytes);	
-	PRINT_MAP_SEGMENT_2("npages: %d\n",npages);
 
 	if (!vman_upages_unmapped(upage, npages))
 		return false;
@@ -139,8 +109,6 @@ vman_map_segment (void *upage, struct file *file, off_t offset, int init_data_by
 		void *cur_upage = (void *)(((uint8_t *)upage) + i*PGSIZE);
 		off_t cur_offset = offset + i*PGSIZE;
 		page_supplement_install_segpage(&t->pst,cur_upage,valid_bytes,file,cur_offset,writable);
-		PRINT_MAP_SEGMENT_2("cur_upage: %p, ",cur_upage);
-		PRINT_MAP_SEGMENT_2("bytes_remaining: %d\n",bytes_remaining);
 		bytes_remaining -= valid_bytes;
 	}
 	ASSERT(bytes_remaining == 0);
@@ -148,22 +116,13 @@ vman_map_segment (void *upage, struct file *file, off_t offset, int init_data_by
 	return true;
 }
 
+/* loads a page into memory. exact behavior depends on type of the page.
+   stack pages are paged in from swap, data segements are either lazy 
+   loaded from file or paged in from swap, and code segements and memory
+    mapped files are always read from file.*/
 void 
 vman_load_page_helper(struct pagesup_entry *pse)
 {
-		/*
-		1. validation:
-				- check that pst is installed, and is not loaded (fte == NULL)
-		2. lock frame
-		3. vman should do clok algorithm and get next frame table entry . 
-		4. Check if frame is currently occupied, if so [ HANDLE EVICTION ]
-		5. Assume ALL NEW PAGES. call palloc from User pool.
-		6. install into page directory.
-		7. update "fte" in supplemental page table
-		8. update fte
-		9.release frame lock
-
-	*/
 
 	ASSERT(pse->ploc != ploc_memory);
 
@@ -175,35 +134,23 @@ vman_load_page_helper(struct pagesup_entry *pse)
   off_t bytes_read;
   switch (pse->ptype) {
   	case ptype_stack:
-  		PRINT_LOAD_PAGE("need to load STACK page\n");
-  		PRINT_LOAD_PAGE_2("upage: %p\n", pse->upage);
-  		PRINT_LOAD_PAGE_2("kpage: %p\n", pse->kpage);
-  		if(pse->ploc == ploc_swap)
-  		{
-  			PRINT_LOAD_PAGE_2("swap index: %d\n", (int)pse->info.s.slot_index);
+  		if(pse->ploc == ploc_swap) {
   			swap_read_slot(pse->info.s.slot_index, kpage);
   		}
   		break;
   	case ptype_segment:
   		ASSERT(pse->ploc != ploc_none);
-  		if (pse->ploc == ploc_file)
-  		{
-  			PRINT_LOAD_PAGE_2("loading segment page from file: %p\n", pse->info.f.file);
-  			PRINT_LOAD_PAGE_2("-> kpage: %p\n", pse->kpage);
+  		if (pse->ploc == ploc_file) {
 	  		bytes_read = file_read_at (pse->info.f.file, kpage, pse->valid_bytes, pse->info.f.offset);
 	  		ASSERT(bytes_read == pse->valid_bytes);
 	  		memset(kpage + pse->valid_bytes,0,PGSIZE-pse->valid_bytes);
 	  		pse->valid_bytes = PGSIZE;
   		}
-  		else if(pse->ploc == ploc_swap)
-  		{
-  			PRINT_LOAD_PAGE("loading segment from swap\n");
-  			PRINT_LOAD_PAGE_2("-> swap slot: %d\n", (int)pse->info.s.slot_index);
+  		else if(pse->ploc == ploc_swap) {
   			swap_read_slot(pse->info.s.slot_index, kpage);
   		}
   		break;
    	case ptype_segment_readonly:
-   		PRINT_LOAD_PAGE("loading READONLY segment\n");
   	case ptype_file:
   		bytes_read = file_read_at (pse->info.f.file, kpage, pse->valid_bytes, pse->info.f.offset);
   		ASSERT(bytes_read == pse->valid_bytes);
@@ -219,6 +166,9 @@ vman_load_page_helper(struct pagesup_entry *pse)
   pagedir_set_accessed (t->pagedir, pse->upage, true);
 }
 
+/* external interface for loading a page into memory. the function acquires the 
+ 	 frame lock and supplementary page table entry lock for the page and 
+ 	 calls vman_load_page_helper */
 void
 vman_load_page(void *upage)
 {
@@ -232,10 +182,13 @@ vman_load_page(void *upage)
 
   lock_release(&pse->lock);
   lock_release(&lock_frame);
-
-  PRINT_LOAD_PAGE("finished loading page\n");
 }
 
+/* pins a page into memory. acquires the frame lock and supplementary page table
+	 entry lock for the page and calls vman_load_page_helper. the lock for the 
+	 page supplemental table entry is held after the function finishes execution,
+	 preventing the page from being evicted to memory.
+	 */
 void
 vman_pin_page(void *upage)
 {
@@ -253,6 +206,8 @@ vman_pin_page(void *upage)
 	lock_release(&lock_frame);
 }
 
+/* unpins a page, allowing it to be evicted from memory. this is achieved
+	 by releasing the lock on the page's supplemental page table entry */
 void
 vman_unpin_page(void *upage)
 {
@@ -261,28 +216,27 @@ vman_unpin_page(void *upage)
 	lock_release(&(pse->lock));
 }
 
+
+/* grows the stack by allocating a new page beneath the 
+	 current page of the stack */
 bool 
 vman_grow_stack(void)
 {
   struct thread *t = thread_current();
   ASSERT (pg_ofs (t->stack_base) == 0);
 
-  PRINT_GROW_STACK("->entering vman_grow_stack:\n");
-  PRINT_GROW_STACK_2("t->stack_base: %p", t->stack_base);
   void *upage = (void *)((uint8_t *)t->stack_base - PGSIZE);
-  /* get rid of this once we EVICTION is implemented */
-  ASSERT(pagedir_get_page(t->pagedir,upage) == NULL);
 
   if (!vman_upages_unmapped(upage, 1))
   	return false;
 
 	page_supplement_install_stackpage(&t->pst,upage);
 	vman_load_page(upage);
-	PRINT_GROW_STACK("->leaving vman_grow_stack.\n");
 	t->stack_base = upage;
   return true;
 }
 
+/* unmaps a memory mapped file mapped to a given user virtual address */
 void
 vman_unmap_file(void *upage, uint32_t file_len)
 {
@@ -303,15 +257,6 @@ vman_unmap_file(void *upage, uint32_t file_len)
 		lock_acquire(&pse->lock);
 		ASSERT( pse != NULL);
 
-		PRINT_UNMAP_FILE_2("cur_upage: %p\n", cur_upage);
-		PRINT_UNMAP_FILE_2("pse->upage: %p\n", pse->upage);
-		PRINT_UNMAP_FILE_2("pse->kpage: %p\n", pse->kpage);
-		PRINT_UNMAP_FILE_2("pse->file: %p\n", pse->info.f.file);
-		PRINT_UNMAP_FILE_2("pse->offset: %d\n", pse->info.f.offset);
-		PRINT_UNMAP_FILE_2("pse->valid_bytes: %d\n", pse->valid_bytes);
-		PRINT_UNMAP_FILE_2("pse->ptype: %d\n", pse->ptype);
-
-		/*frame_evict(pse);*/
 
 		/* write to disk if necessary */
 		
@@ -338,6 +283,9 @@ vman_unmap_file(void *upage, uint32_t file_len)
 	}
 }
 
+
+/* destroy the virtual memory of a process. free all frames currently occupied
+	 and all data structures associated with memory management. */
 void
 vman_free_all_pages(void)
 {
--- pintos/src/vm/vman.h
+++ pintos/src/vm/vman.h
@@ -8,8 +8,9 @@
 
 void vman_init(void);
 bool vman_upages_unmapped(void *upage_head, int npages);
-bool vman_map_segment (void *upage, struct file *file, off_t offset, int init_data_bytes, 
-									int uninit_data_bytes, bool writable);
+bool vman_map_segment (void *upage, struct file *file, off_t offset, 
+											 int init_data_bytes, int uninit_data_bytes, 
+											 bool writable);
 bool vman_map_file(void *upage, struct file *file, uint32_t file_len);
 void vman_unmap_file(void *upage, uint32_t file_len);
 bool vman_grow_stack(void);
